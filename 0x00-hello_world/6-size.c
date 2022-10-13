812743f4ecb64f885a96e62329383b8cc78132f5
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-13 17:49:09 +0000

    low level task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %d bytes(s)", sizeof(c));
-	printf("Size of a int: %d bytes(s)", sizeof(i));
-	printf("Size of a long int: %d bytes(s)", sizeof(li));
-	printf("Size of a long long int: %d bytes(s)", sizeof(lli));
-	printf("Size of a float: %d bytes(s)", sizeof(f));
+	printf("Size of a char: %d bytes(s)/n", sizeof(c));
+	printf("Size of a int: %d bytes(s)/n", sizeof(i));
+	printf("Size of a long int: %d bytes(s)/n", sizeof(li));
+	printf("Size of a long long int: %d bytes(s)/n", sizeof(lli));
+	printf("Size of a float: %d bytes(s)/n", sizeof(f));
 
 	return (0);
 }
