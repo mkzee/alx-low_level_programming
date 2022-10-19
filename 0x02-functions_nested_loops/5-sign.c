2db07611c383c4b29b0ff4ab086940c5e3e20a09
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-19 09:40:28 +0000

    function task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - prints a sign base on different conditions
+ *
+ * @n: n represent an integer passed into the print_sign function
+ *
+ * Return: returns 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+		_putchar('0');
+		return (1);
+	else if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n < 0)
+		_putchar('-')
+		return (-1)
+}
