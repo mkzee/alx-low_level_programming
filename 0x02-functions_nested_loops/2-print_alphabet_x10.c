a8fc5534f58d8b828689bbb3ab37e4a0895f6d53
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-19 04:03:37 +0000

    function task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ * Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char ch;
+
+	while (char < 10)
+	{
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
