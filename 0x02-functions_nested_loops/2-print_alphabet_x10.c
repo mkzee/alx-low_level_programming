232dbfa666af5c5851afd09c5e641b3b39c0408e
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-19 04:06:42 +0000

    function task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char ch;
 
-	while (char < 10)
+	while (count < 10)
 	{
 		for (ch = 'a'; ch < 'z'; ch++)
 		{
 			_putchar(ch);
 		}
+		count++;
 		_putchar('\n');
 	}
 }
