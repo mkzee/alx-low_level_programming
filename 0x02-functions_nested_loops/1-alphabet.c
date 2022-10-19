1bb2e06558b94fd16cc8458fa17bf01631d16fd6
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-19 03:18:51 +0000

    function task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - prints letters of alphabets
  * Return: 0
  */
 
 void print_alphabet(void)
 {
 	char ch = 'a';
-	while(ch <= 'z')
+
+	while (ch <= 'z')
 	{
 		_putchar(ch);
 		_putchar('\n');
-		ch ++;
+		ch++;
 	}
-
-	return (0);
 }
