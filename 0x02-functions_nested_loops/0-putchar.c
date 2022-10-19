bc4e76f10ef9b30ef5b028641cad8f64566389e7
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-19 02:57:33 +0000

    function task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Entry point
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	_putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
-        _putchar('h');
-        _putchar('a');
+	_putchar('h');
+	_putchar('a');
 	_putchar('r');
-        _putchar('\n');
-	return 0;
+	_putchar('\n');
+	return (0);
 }
