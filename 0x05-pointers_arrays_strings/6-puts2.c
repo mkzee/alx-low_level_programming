d525c306a94401a6da6e3a522cc8968dc1f8a671
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-24 08:40:08 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with first character, followed by /n
+ *
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i;
+	char l;
+
+	for (i = 0; *(str + i) != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			l = *(str + i);
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
