69ea3ffb335bafd43d5ad446f5e2dd2fe6b88844
Emeka Mezue <mezueemeka23@gmail.com>
2022-10-24 08:27:02 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string to be counted
+ *
+ * Return: length of a string
+ */
+
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+			count++;
+			s++;
+	}
+	return (count);
+}
