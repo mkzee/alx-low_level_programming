099783df75bd8863ef3e4614b468fce2f26a7a76
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-10-25 15:57:02 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ *
+ * Return: product of a and b
+ * @a: first value to be multiplied
+ * @b: second velue to be multiplied
+ */
+
+int mul(int a, int b)
+{
+
+	int result = a * b;
+
+	return (result);
+}
