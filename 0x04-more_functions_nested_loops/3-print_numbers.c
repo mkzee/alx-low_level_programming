3bfe4c274670228ee5e48d5720af61b17913980a
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-10-25 18:15:44 +0100

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers 0 through 9
- * Return: void
-*/
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ */
 
 void print_numbers(void)
 {
+	int i;
 
-	int i = 0;
-
-	while (i <= 9)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar('0' + i);
-		i++;
+		_putchar(i);
 	}
 
 	_putchar('\n');
