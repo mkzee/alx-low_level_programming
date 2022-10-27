060bad662ba87c85ccc7b05a19cd8fea6090b50a
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-10-27 11:33:03 +0100

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+<include> "main.h"
+
+/**
+ **_strcat - concatenates strings passed in the function
+ * by apppending src to dest
+ * Return: returns dest
+ * @*dest is a string and value to be updated
+ * @*src is a string and the value to be appended
+ */
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j = 6;
+
+	for (i = 0; i < sizeof(src) / sizeof(src[i]); i ++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+
+	return (dest)
+}
