067392c758321eeea33fde7e58e069ea12707e34
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-10-28 01:47:00 +0100

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
 
-	int i;
-	int j;
 
-	for (i = 0; dest[1] != '\0')
+	for (i = 0; dest[i] != '\0';)
 		i++;
 
-	for (j = 0; j < n; j++)
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
 		dest[i + j] = src[j];
 
+	dest[i + j] = '\0';
 	return (dest);
 }
