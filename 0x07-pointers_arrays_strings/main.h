8a223a8af6814a67d64b73d4f403b3dc8ce8f2df
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-01 04:27:09 +0100

    task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,1 +1,8 @@
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
