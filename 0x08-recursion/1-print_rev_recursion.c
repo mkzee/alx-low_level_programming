8c7c59fa21c0f03db46191941de76cce21186bb5
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-03 02:08:44 +0100

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,11 +5,11 @@
  * @s: the string to be reversed
  */
 
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	)
+	}
 }
