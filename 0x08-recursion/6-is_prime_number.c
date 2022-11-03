593fac35d3f9fcffd426f042afe8637d56a2c7ae
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-03 14:29:27 +0100

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a n is a prime number
+ * @n: is number to check
+ * Return: 1 if n is prime and 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+
+
+/**
+ * prime - checks if n is a prime number
+ * @n: is number to check
+ * @x: values to test
+ * Return: recursively until base cases are met
+ */
+
+int prime(int n, int x)
+{
+	if (n <= 0)
+		return (0);
+
+	if (n == 1)
+		return (1);
+
+	if (!(n % x) && n == x)
+		return (1);
+
+	if (!(n % x))
+		return (0);
+
+	return (prime(n, x + 1));
+}
