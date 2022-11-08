f72a5fdbd72abf4bf91948468dab3c852f0f55ec
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-08 09:30:29 +0100

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always return zero (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
