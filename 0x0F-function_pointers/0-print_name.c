e9e53598add2ea0a81268775c28b1f51e5a0864b
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-17 01:34:24 +0100

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,11 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * print_name - prints a name
- * @name: is name passed to function
- * @f: callback function
- * Return: void
+ * print_name - prints name using function pointer as callback
+ * @name: name to print
+ * @f: function pointer
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == 0 || f == 0)
