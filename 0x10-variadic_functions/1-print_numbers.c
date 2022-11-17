0c8790b84c8d7aabedb9a0973f66baa6ed96b247
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-18 00:09:10 +0100

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers to stdout
+ * @separator: character to print after each number
+ * @n: number of parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+
+		if (separator == NULL)
+		{
+			printf("%d", x);
+		}
+
+		printf("%d%s", x, separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
