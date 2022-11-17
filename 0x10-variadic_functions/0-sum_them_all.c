628cea6c7586b0989982e06cc353e34945cb1289
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-17 23:14:27 +0100

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,22 +9,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
 	if (n == 0)
 		return (0);
 
-	unsigned int i;
-	int sum = 0;
-
-	va_list arg;
-
-	va_start(arg, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(arg, int);
+		sum += va_arg(args, int);
 	}
 
-	va_end(arg);
+	va_end(args);
 
 	return (sum);
 }
