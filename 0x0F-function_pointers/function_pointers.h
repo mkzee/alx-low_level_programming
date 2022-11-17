54c7dc9df98b556c2c92664f67193aafde249fbf
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-17 01:36:55 +0100

    task 0:

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stddef.h>
-#include <stdio.h>
-
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
