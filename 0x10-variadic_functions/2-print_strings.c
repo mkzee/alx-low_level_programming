16dec130f6b2d1831951757e46d4f8ab92b74c96
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-18 01:38:49 +0100

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,10 +1,28 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *s;
 	va_list args;
 
-	if (n ==
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char*);
+
+		if(s)
+			printf("%s", s);
+		else
+			printf("(nil)");
+
+		if (i != n - i && separator)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
 }
