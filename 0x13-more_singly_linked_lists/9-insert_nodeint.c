0078fb64170fa9c77a53d6ea5a5996c51c9fbe43
Emeka Mezue <thealgorhythm@DESKTOP-KSPC9PD.localdomain>
2022-11-30 00:49:24 +0100

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,33 +1,40 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * delete_nodeint_at_index - delete node at index
+ * insert_nodeint_at_index - insert node at given pos
  * @head: head node
- * @index: index node
- * Return: 1 succed -1 fails
+ * @idx: index pos
+ * @n: new node data
+ * Return: address of new node
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *prev, *node;
+	listint_t *node, *prev;
 
-	if (!*head)
-		return (-1);
+	if (!head)
+		return (0);
 
-	prev = get_nodeint_at_index(*head, index - 1);
-	node = get_nodeint_at_index(*head, index);
+	if (idx == 0)
+	{
+		node = add_nodeint(head, n);
+		return (node);
+	}
+
+	node = malloc(sizeof(listint_t));
 	if (!node)
-		return (-1);
-	if (index != 0)
+		return (0);
+	node->n = n;
+
+	prev = get_nodeint_at_index(*head, idx - 1);
+	if (!prev)
 	{
-		prev->next = node->next;
-		node->next = 0;
+		free(node);
+		return (0);
 	}
-	else
-	{
-		*head = node->next;
-	}
-	free(node);
-	return (1);
+
+	node->next = prev->next;
+	prev->next = node;
+	return (node);
 }
 /**
  * get_nodeint_at_index - get node at index
@@ -38,16 +45,39 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+	listint_t *node;
+
+	node = head;
+
+	if (!node)
+		return (0);
+
+	while (node)
+	{
+		if (index == i)
+			return (node);
+		i++;
+		node = node->next;
+	}
+	return (0);
+}
+/**
+ * add_nodeint - add not at start
+ * Return: pointer to node
+ * @head: head node
+ * @n: new node data
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *node;
 
 	if (!head)
 		return (0);
-
-	while (head)
-	{
-		if (index == i)
-			return (head);
-		i++;
-		head = head->next;
-	}
-	return (0);
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (0);
+	node->n = n;
+	node->next = *head;
+	*head = node;
+	return (node);
 }
